8cf6eaeeb5ee66431473ee18e8eadc07b4a917e0
Ben Cross <ssorc3@gmail.com>
2018-01-29 17:57:16 +0000

    MySQL server

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,11 +1,13 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
 # --- !Ups
 
-create table "users" (
-  "userID" varchar not null primary key,
-  "username" varchar not null,
+create table users (
+  "userID" varchar(120) not null primary key,
+  "username" varchar(120) not null,
   "hash" varchar (80) not null
 );
 
 # --- !Downs
 
-drop table "users" if exists;
+drop table users;
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,19 +1,21 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
 # --- !Ups
 
-CREATE TABLE "devices"(
-  "deviceID" VARCHAR PRIMARY KEY,
-  "userID" VARCHAR,
-  "deviceName" VARCHAR
+CREATE TABLE devices(
+  "deviceID" VARCHAR(120) PRIMARY KEY,
+  "userID" VARCHAR(120),
+  "deviceName" VARCHAR(120)
 );
 
-CREATE TABLE "temperature"(
+CREATE TABLE temperature(
   "id" INT AUTO_INCREMENT PRIMARY KEY,
-  "deviceID" VARCHAR,
+  "deviceID" VARCHAR(120),
   "timestamp" BIGINT,
   "value" DECIMAL
 );
 
 # --- !Downs
 
-DROP TABLE "devices";
-DROP TABLE "temperature";
+DROP TABLE devices;
+DROP TABLE temperature;
