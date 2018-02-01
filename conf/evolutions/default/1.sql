10358c26469ce4f1032d0d1979d325cf426df0e2
Unknown <ssorc3@gmail.com>
2018-02-01 10:43:12 +0000

    Fix database

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,12 +2,12 @@
 
 # --- !Ups
 
-create table "users" (
-  "userID" varchar(120) not null primary key,
-  "username" varchar(120) not null,
-  "hash" varchar (80) not null
+create table users (
+  userID varchar(120) not null primary key,
+  username varchar(120) not null,
+  hash varchar (80) not null
 );
 
 # --- !Downs
 
-drop table "users";
+drop table users;
