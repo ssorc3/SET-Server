2e49e55ef27dbf7911e98b629b6997317f914711
Ben Cross <ssorc3@gmail.com>
2018-02-13 14:44:38 +0000

    Added additional commands to SETLang

--- app/setLang/Parser.scala
+++ app/setLang/Parser.scala
@@ -45,7 +45,7 @@ class Parser extends StandardTokenParsers
                                 "text"  ^^^ Text()                           |
                                 "notification" ^^^ Notification()            |
                                 "kettle" ^^^ Kettle()                        |
-                                "lightSetting" ~> lightCommand ^^^ {case a => Lights(a)}                |
+                                "lights" ~> lightCommand ^^^ {case a => Lights(a)}                |
                                 ("lightSetting" ~> bool) ~ (", " ~> numericLit) ~ (", " ~> numericLit) ^^ {case a ~ b ~ c => LightSetting(a, b.toInt, c.toInt)}
 
 
