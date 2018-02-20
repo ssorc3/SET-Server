1107cac7581aed3dbe9cce1c6f0241312f04191e
Ben Cross <ssorc3@gmail.com>
2018-02-20 16:04:00 +0000

    on

--- app/controllers/ActuatorController.scala
+++ app/controllers/ActuatorController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.Inject
 
-import auth.SecuredAuthenticator
+import auth.{JWTUtil, SecuredAuthenticator}
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import repositories.DeviceRepository
@@ -10,7 +10,7 @@ import websockets.WebSocketManager
 
 import scala.concurrent.ExecutionContext
 
-class ActuatorController @Inject()(cc: ControllerComponents, auth: SecuredAuthenticator, devices: DeviceRepository)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class ActuatorController @Inject()(cc: ControllerComponents, auth: SecuredAuthenticator, devices: DeviceRepository, jWTUtil: JWTUtil)(implicit ec: ExecutionContext) extends AbstractController(cc) {
   def setKettleIP(): Action[JsValue] = auth.JWTAuthentication.async(parse.json){ implicit request =>
     val userID = request.user.userID
     val kettleIP: String = (request.body \ "ip").asOpt[String].getOrElse("")
@@ -33,6 +33,18 @@ class ActuatorController @Inject()(cc: ControllerComponents, auth: SecuredAuthen
       })
       Ok
     }
+  }
+
+  def boilKettleAssistant(): Action[JsValue] = Action(parse.json) {implicit request =>
+    val userID = (request.body \ "userID").asOpt[String].getOrElse("")
+    devices.getUserBridges(userID).map {x =>
+      x.foreach(b => WebSocketManager.getConnection(b) match {
+        case Some(c) =>
+          c ! "kettle"
+        case _ => Ok(<h1>We're having a problem contacting your bridge. Make sure it is connected.</h1>)
+      })
+    }
+    Ok
   }
 
   def setLightIP(): Action[JsValue] = auth.JWTAuthentication.async(parse.json){ implicit request =>
