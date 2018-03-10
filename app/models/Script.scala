65723a4113b164e5dff272980201f197507f5325
Ben Cross <ssorc3@gmail.com>
2018-03-10 14:23:49 +0000

    Add script to DTO

--- app/models/Script.scala
+++ app/models/Script.scala
@@ -6,14 +6,20 @@ import play.api.mvc.Codec
 
 case class Script(userID: String, scriptName: String, script: String, lastRun: Long)
 
+case class ScriptDTO(scriptName: String, script: String)
+
 object Script{
   implicit def writeableSeq(implicit codec: Codec): Writeable[Seq[Script]] = {
-    Writeable(data => codec.encode(Json.toJson(data).toString))
+    Writeable(data => codec.encode(Json.toJson(data.map(toDTO)).toString))
   }
 
   implicit def contentTypeSeq(implicit codec: Codec): ContentTypeOf[Seq[Script]] = {
     ContentTypeOf(Some(ContentTypes.TEXT))
   }
 
-  implicit val scriptFormat: OFormat[Script] = Json.format[Script]
+  implicit val scriptFormat: OFormat[ScriptDTO] = Json.format[ScriptDTO]
+
+  def toDTO(script: Script): ScriptDTO = {
+    ScriptDTO(script.scriptName, script.script)
+  }
 }
