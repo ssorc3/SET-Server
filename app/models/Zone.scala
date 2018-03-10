838d9a43b0baada774ba0b0000579116d8e203db
Ben Cross <ssorc3@gmail.com>
2018-03-10 18:42:59 +0000

    Added writeable zone

--- app/models/Zone.scala
+++ app/models/Zone.scala
@@ -1,3 +1,20 @@
 package models
 
+import play.api.http
+import play.api.http.{ContentTypeOf, ContentTypes, Writeable}
+import play.api.libs.json.{Json, OFormat}
+import play.api.mvc.Codec
+
 case class Zone(id: Int, userID: String, name: String)
+
+object Zone {
+  implicit def writeableSeq(implicit codec: Codec): Writeable[Seq[Zone]] = {
+    http.Writeable(data => codec.encode(Json.toJson(data).toString))
+  }
+
+  implicit def contentTypeSeq(implicit codec: Codec): ContentTypeOf[Seq[Zone]] = {
+    ContentTypeOf(Some(ContentTypes.TEXT))
+  }
+
+  implicit val deviceFormat: OFormat[Zone] = Json.format[Zone]
+}
