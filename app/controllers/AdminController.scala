7dafd1672874df7845734aea74a6450ea7c67209
Ben Cross <ssorc3@gmail.com>
2018-03-12 17:53:05 +0000

    a

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -12,10 +12,4 @@ import scala.concurrent.ExecutionContext
 class AdminController @Inject() (assets: Assets, errorHandler: HttpErrorHandler, config: Configuration, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc)
 {
   def index: Action[AnyContent] = assets.at("index.html")
-
-  def assetOrDefault(resource: String): Action[AnyContent] = if (resource.startsWith(config.get[String]("apiPrefix"))){
-    Action.async(r => errorHandler.onClientError(r, NOT_FOUND, "Not found"))
-  } else {
-    if (resource.contains(".")) assets.at(resource) else index
-  }
 }
