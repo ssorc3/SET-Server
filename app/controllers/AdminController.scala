9a8c03772f1ee5f5e417a2d130e3895c949c52dd
Ben Cross <ssorc3@gmail.com>
2018-03-12 14:32:25 +0000

    Add front-end

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -2,14 +2,20 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
+import play.api.Configuration
+import play.api.http.HttpErrorHandler
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class AdminController @Inject() (cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc)
+class AdminController @Inject() (assets: Assets, errorHandler: HttpErrorHandler, config: Configuration, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc)
 {
-  def index(): Action[AnyContent] = Action{ implicit request =>
-    Ok(views.html.admin())
+  def index: Action[AnyContent] = assets.at("index.html")
+
+  def assetOrDefault(resource: String): Action[AnyContent] = if (resource.startsWith(config.get[String]("apiPrefix"))){
+    Action.async(r => errorHandler.onClientError(r, NOT_FOUND, "Not found"))
+  } else {
+    if (resource.contains(".")) assets.at(resource) else index
   }
 }
