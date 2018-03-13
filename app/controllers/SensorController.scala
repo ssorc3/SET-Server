d3aadd7e23572f405c9a24d753d55b03d9fbf437
Ben Cross <ssorc3@gmail.com>
2018-03-13 16:59:37 +0000

    Add motion detecting zones

--- app/controllers/SensorController.scala
+++ app/controllers/SensorController.scala
@@ -186,8 +186,10 @@ class SensorController @Inject()(cc: MessagesControllerComponents, auth: Secured
   private def getUserCurrentZone(username: String): String = {
     val request: WSRequest = ws.url("localhost:8000/location?group=" + username + "&user=" + username)
     request.get().map(response =>
-      if(response.status == 200)
+      if(response.status == 200) {
+        println("Got response")
         return (response.json \ "users" \ username \ "location").as[String]
+      }
     )
     ""
   }
