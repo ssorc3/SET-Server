5ce4e870d2548937798def749616ff787f4cbc7b
Ben Cross <ssorc3@gmail.com>
2018-03-13 17:47:03 +0000

    Add motion detecting zones

--- app/controllers/SensorController.scala
+++ app/controllers/SensorController.scala
@@ -209,7 +209,7 @@ class SensorController @Inject()(cc: MessagesControllerComponents, auth: Secured
     request.get().map(response => {
       println("Response: " + response.status)
       println("Message: " + (response.json \ "message").as[String])
-      println("Response success: " + (response.json \ "users"))
+      println("Response success: " + (response.json \ "users" \ username \\ "location"))
       if (response.status == 200) {
         if ((response.json \ "success").as[Boolean]) {
           (response.json \ "users" \ username \ "location").as[String]
