91a02b15d8ea4f29dcb1aa9cae389d15d15ae929
Ben Cross <ssorc3@gmail.com>
2018-03-13 14:06:36 +0000

    Change lightSetting command

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,3 +1,20 @@
 package models
 
+import play.api.http
+import play.api.http.{ContentTypeOf, ContentTypes, Writeable}
+import play.api.libs.json.{Json, OFormat}
+import play.api.mvc.Codec
+
 case class User(userID: String, username: String, hash: String)
+
+object User {
+  implicit def writeableSeq(implicit codec: Codec): Writeable[Seq[User]] = {
+    http.Writeable(data => codec.encode(Json.toJson(data).toString))
+  }
+
+  implicit def contentTypeSeq(implicit codec: Codec): ContentTypeOf[Seq[User]] = {
+    ContentTypeOf(Some(ContentTypes.TEXT))
+  }
+
+  implicit val deviceFormat: OFormat[User] = Json.format[User]
+}
