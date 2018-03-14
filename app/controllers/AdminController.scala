c0de79e935fcc4488df0e08618341a0ea3cf8d36
Ben Cross <ssorc3@gmail.com>
2018-03-14 18:27:06 +0000

    Fix ws.url

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -39,7 +39,7 @@ class AdminController @Inject()(cc: ControllerComponents, auth: SecuredAuthentic
     }
   }
 
-  def Cors(anything: String) = Action(parse.default){
+  def Cors() = Action(parse.default){
     Ok
   }
 }
